c343e4d8367441d1aec98265f4ee2fc058cb050a
Ivan Glushchenko <ivang.github@outlook.com>
2013-01-21 20:16:34 -0500

    support for internal classes (in progress)

--- CleanIt/CleanRegions.fs
+++ CleanIt/CleanRegions.fs
@@ -9,10 +9,17 @@ open Roslyn.Services.CSharp
 open Roslyn.Services.Formatting
 
 
-let (|RegionDirective|_|) (expr:SyntaxTrivia) =    if expr.Kind = SyntaxKind.RegionDirectiveTrivia then Some(expr) else None
-let (|EndRegionDirective|_|) (expr:SyntaxTrivia) = if expr.Kind = SyntaxKind.EndRegionDirectiveTrivia then Some(expr) else None
-let (|EndOfLine|_|) (expr:SyntaxTrivia) =          if expr.Kind = SyntaxKind.EndOfLineTrivia then Some(expr) else None
-let (|Whitespace|_|) (expr:SyntaxTrivia) =         if expr.Kind = SyntaxKind.WhitespaceTrivia then Some(expr) else None
+let (|RegionDirective|_|) (expr:SyntaxTrivia) =                  if expr.Kind = SyntaxKind.RegionDirectiveTrivia then Some(expr) else None
+let (|EndRegionDirective|_|) (expr:SyntaxTrivia) =               if expr.Kind = SyntaxKind.EndRegionDirectiveTrivia then Some(expr) else None
+let (|EndOfLine|_|) (expr:SyntaxTrivia) =                        if expr.Kind = SyntaxKind.EndOfLineTrivia then Some(expr) else None
+let (|Whitespace|_|) (expr:SyntaxTrivia) =                       if expr.Kind = SyntaxKind.WhitespaceTrivia then Some(expr) else None
+let (|ConstructorDeclaration|_|) (expr:SyntaxNode) =             if expr :? ConstructorDeclarationSyntax then Some(expr :?> ConstructorDeclarationSyntax) else None
+let (|EventFieldDeclaration|_|) (expr:SyntaxNode) =              if expr :? EventFieldDeclarationSyntax then Some(expr :?> EventFieldDeclarationSyntax) else None
+let (|FieldDeclaration|_|) (expr:SyntaxNode) =                   if expr :? FieldDeclarationSyntax then Some(expr :?> FieldDeclarationSyntax) else None
+let (|PropertyDeclaration|_|) (expr:SyntaxNode) =                if expr :? PropertyDeclarationSyntax then Some(expr :?> PropertyDeclarationSyntax) else None
+let (|MethodDeclaration|_|) (expr:SyntaxNode) =                  if expr :? MethodDeclarationSyntax then Some(expr :?> MethodDeclarationSyntax) else None
+let (|ClassDeclaration|_|) (expr:SyntaxNode) =                   if expr :? ClassDeclarationSyntax then Some(expr :?> ClassDeclarationSyntax) else None
+
 
 let eol() = Syntax.EndOfLine("\r\n", false)
 let tab() = Syntax.Whitespace("\t", false)
@@ -47,6 +54,40 @@ let getName = function
    | Methods    -> "Methods"
    | Other      -> "Other"
 
+
+type MemberType =
+   | Ctor     
+   | Event    
+   | Field    
+   | Property 
+   | Method   
+   | Construct
+   | Class
+ 
+let toMemberType (node: SyntaxNode) =
+   match node with
+   | :? ConstructorDeclarationSyntax -> Ctor
+   | :? EventFieldDeclarationSyntax  -> Event
+   | :? FieldDeclarationSyntax       -> Field
+   | :? PropertyDeclarationSyntax    -> Property
+   | :? MethodDeclarationSyntax      -> Method
+   | :? ClassDeclarationSyntax       -> Class
+   | _                               -> raise (new System.NotSupportedException())
+
+let toSignature (node: SyntaxNode) =
+   let join (l:string array) = if l.Length = 0 then "" else " " + System.String.Join(":", l)
+   let getParams (pSyntax: ParameterListSyntax) = (pSyntax.Parameters |> Seq.map (fun t -> t.Type.ToString())).ToArray() |> join
+   let getModifiers (tokens: SyntaxTokenList) = (tokens |> Seq.map (fun t -> t.ValueText)).ToArray() |> join
+   match node with
+   | ConstructorDeclaration(n) -> ".ctor" + (getParams n.ParameterList) + (getModifiers n.Modifiers)
+   | EventFieldDeclaration(n)  -> n.Declaration.Variables.First().Identifier.ValueText
+   | FieldDeclaration(n)       -> n.Declaration.Variables.First().Identifier.ValueText
+   | PropertyDeclaration(n)    -> n.Identifier.ValueText
+   | MethodDeclaration(n)      -> n.Identifier.ValueText + (getParams n.ParameterList)
+   | ClassDeclaration(n)       -> n.Identifier.ValueText
+   | _                         -> raise (new System.NotSupportedException())
+
+
 type RegionRemover() = 
    inherit SyntaxRewriter(false)
 
@@ -68,6 +109,12 @@ type RegionRemover() =
       | (false, false) -> base.VisitToken token
 
 
+let rec getClassName (node: SyntaxNode) =
+   if node = null then null
+   else if node :? ClassDeclarationSyntax then (node :?> ClassDeclarationSyntax).Identifier.ValueText
+   else getClassName node.Parent
+
+
 type MemberCollector() = 
    inherit SyntaxRewriter(false)
 
@@ -81,6 +128,32 @@ type MemberCollector() =
 
    let isStatic (tokens: SyntaxTokenList) = tokens |> Seq.exists (fun t -> t.ValueText = "static")
    let isPublic (tokens: SyntaxTokenList) = tokens |> Seq.exists (fun t -> t.ValueText = "public")
+
+   let mutable members = List.empty<MemberDeclarationSyntax>
+
+   let membersMap = lazy (
+      let toMemberMap (m: MemberDeclarationSyntax seq) = m |> Seq.map (fun t -> (toSignature t, t)) |> Seq.sortBy (fun (t, k) -> t) |> Map.ofSeq
+      let toClassMap (m: MemberDeclarationSyntax seq) =  
+         let members = m |> Seq.groupBy (fun t -> toMemberType t) |> Seq.map (fun (k, s) -> (k, toMemberMap s)) |> Map.ofSeq
+         if members.ContainsKey Field = false || members.ContainsKey Property = false then members
+         else
+            let getDependencyProperty name =
+               let fieldName = name + "Property"
+               if members.[Field].ContainsKey fieldName = false then None else
+                  let fp = members.[Field].[fieldName] :?> FieldDeclarationSyntax
+                  if fp.Declaration.Type.ToString() = "DependencyProperty" && (isStatic fp.Modifiers) then Some(fp :> MemberDeclarationSyntax) else None
+            let getAutoGeneratedProperty name =
+               let fieldName = "p_" + name
+               if members.[Field].ContainsKey fieldName = false then None else
+                  let fp = members.[Field].[fieldName] :?> FieldDeclarationSyntax
+                  let pp = members.[Property].[name] :?> PropertyDeclarationSyntax
+                  if fp.Declaration.Type.ToString() = pp.Type.ToString() then Some(fp :> MemberDeclarationSyntax) else None
+            let dpCandidates = members.[Property] |> Map.toSeq |> Seq.map (fun (k, v) -> (v, getDependencyProperty k)) |> Seq.filter (fun (k, v) -> v.IsSome) |> Seq.collect (fun (k, v) -> [k; v.Value])
+            let agCandidates = members.[Property] |> Map.toSeq |> Seq.map (fun (k, v) -> (v, getAutoGeneratedProperty k)) |> Seq.filter (fun (k, v) -> v.IsSome) |> Seq.collect (fun (k, v) -> [k; v.Value])
+            m |> Seq.groupBy (fun t -> toMemberType t) |> Seq.map (fun (k, s) -> (k, toMemberMap s)) |> Map.ofSeq
+            members
+      members |> Seq.groupBy (fun t -> getClassName t) |> Seq.map (fun (k, s) -> (k, toClassMap s)) |> Map.ofSeq
+   )
 
    let mutable constructs = Map.empty<string, Construct>
    let mutable constructors = Map.empty<string, ConstructorDeclarationSyntax>
@@ -124,6 +197,14 @@ type MemberCollector() =
          Some(l)
       | None -> None
 
+//   let collectMember (node: SyntaxNode) =
+//      let mType = toMemberType node
+//      let cName = getClassName node
+//      if members.ContainsKey cName  = false then members <- members.Add (cName, Map.empty)
+//      if members.[cName].ContainsKey mType = false then members.[cName] <- 
+    
+   member x.Members with get() = membersMap
+
    member x.Ctors             with get() = toSyntaxList constructors |> markRegionBorders Ctors
    member x.Events            with get() = toSyntaxList events |> markRegionBorders Events
    member x.Fields            with get() = toSyntaxList fields |> markRegionBorders Fields
@@ -135,39 +216,63 @@ type MemberCollector() =
    member x.Methods           with get() = toSyntaxList methods |> markRegionBorders Methods
    member x.OpenRegionMarkers with get() = openRegionMarkers
 
+   override x.VisitClassDeclaration node =
+      let rec getParentClass (n: SyntaxNode) = if n = null then null else match n.Parent with | ClassDeclaration(c) -> c | _ -> getParentClass n.Parent 
+      let parentClass = getParentClass node
+      if parentClass = null then base.VisitClassDeclaration node 
+      else 
+         members <- (node :> MemberDeclarationSyntax) :: members
+         null
+
    override x.VisitConstructorDeclaration node = 
-      constructors <- constructors.Add (node.ParameterList.ChildNodes().Count().ToString() + getModifiers(node.Modifiers), node)
+      members <- (node :> MemberDeclarationSyntax) :: members
+      //constructors <- constructors.Add (node.ParameterList.ChildNodes().Count().ToString() + getModifiers(node.Modifiers), node)
       null
 
    override x.VisitEventFieldDeclaration node =
-      events <- events.Add (node.Declaration.Variables.First().Identifier.ValueText, node)
+      members <- (node :> MemberDeclarationSyntax) :: members
+      //events <- events.Add (node.Declaration.Variables.First().Identifier.ValueText, node)
       null
 
    override x.VisitFieldDeclaration node =
-      let name = node.Declaration.Variables.First().Identifier.ValueText
-      fields <- fields.Add (name, node)
-      collectConstructs (if name.EndsWith "Property" then name.Substring(0, name.Length - 8) else if name.StartsWith "p_" then name.Substring(2) else name)
+      members <- (node :> MemberDeclarationSyntax) :: members
+      //let name = node.Declaration.Variables.First().Identifier.ValueText
+      //fields <- fields.Add (name, node)
+      //collectConstructs (if name.EndsWith "Property" then name.Substring(0, name.Length - 8) else if name.StartsWith "p_" then name.Substring(2) else name)
       null
 
    override x.VisitPropertyDeclaration node =
-      properties <- properties.Add (node.Identifier.ValueText, node)
-      collectConstructs node.Identifier.ValueText
+      members <- (node :> MemberDeclarationSyntax) :: members
+      //properties <- properties.Add (node.Identifier.ValueText, node)
+      //collectConstructs node.Identifier.ValueText
       null
 
    override x.VisitMethodDeclaration node =
-      methods <- methods.Add (node.Identifier.ValueText + getSignature(node.ParameterList), node)
+      members <- (node :> MemberDeclarationSyntax) :: members
+      //methods <- methods.Add (node.Identifier.ValueText + getSignature(node.ParameterList), node)
       null
 
 
-type MemberArranger(collector:MemberCollector) = 
+type MemberArranger(collector: MemberCollector) = 
    inherit SyntaxRewriter(false)
 
    override x.VisitClassDeclaration node =
-      let extend (members:SyntaxList<MemberDeclarationSyntax> option) (node:ClassDeclarationSyntax) =
-         match members with
-         | Some(list) -> node.WithMembers(Syntax.List(list.Concat(node.Members)))
-         | None       -> node
-      base.VisitClassDeclaration (node |> extend collector.Methods |> extend collector.Properties |> extend collector.Fields |> extend collector.Events |> extend collector.Ctors)
+      let sgn = toSignature node
+      if collector.Members.Value.ContainsKey sgn then
+         let classMembers = collector.Members.Value.[sgn]
+         let extend (mType: MemberType) (node: ClassDeclarationSyntax) = 
+            if classMembers.ContainsKey mType then
+               let members = classMembers.[mType] |> Map.toList |> List.map snd
+               node.WithMembers(Syntax.List(members.Concat(node.Members)))
+            else node
+         node |> extend Class |> extend Method |> extend Property |> extend Field |> extend Event |> extend Ctor :> SyntaxNode
+      else base.VisitClassDeclaration node
+
+//      let extend (members:SyntaxList<MemberDeclarationSyntax> option) (node:ClassDeclarationSyntax) =
+//         match members with
+//         | Some(list) -> node.WithMembers(Syntax.List(list.Concat(node.Members)))
+//         | None       -> node
+//      base.VisitClassDeclaration (node |> extend collector.Methods |> extend collector.Properties |> extend collector.Fields |> extend collector.Events |> extend collector.Ctors)
 
 
 type RegionRewriter(collector:MemberCollector) = 
@@ -182,7 +287,7 @@ type RegionRewriter(collector:MemberCollector) =
       | EndOfLine(t1) :: Whitespace(t2) :: tl -> 
          let trivia = [eol(); tab(); region; eol(); eol(); t2] @ tl
          token.WithLeadingTrivia(trivia)
-      | Whitespace(t1) :: tl -> token.WithLeadingTrivia([Syntax.Whitespace(t1.ToFullString()); tab(); region] @ [eol(); eol()] @ formattedList)
+      | Whitespace(t1) :: tl -> token.WithLeadingTrivia([Syntax.Whitespace(t1.ToFullString()); region] @ [eol(); eol()] @ formattedList)
       | _ -> token.WithLeadingTrivia(eol() :: tab() :: region :: formattedList)
 
    let closeRegion (token: SyntaxToken) (region: RegionType) = 
