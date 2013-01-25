1895005241390f95b5ce14cd87127450b27a880e
Ivan Glushchenko <ivang.github@outlook.com>
2013-01-24 19:35:55 -0500

    support for regions inside internal classes

--- CleanIt/CleanUsings.fs
+++ CleanIt/CleanUsings.fs
@@ -12,11 +12,17 @@ type UsingDirectiveRewrite() =
 
    let usings = System.Collections.Generic.List<UsingDirectiveSyntax>()
 
-   member visitor.getCollectedUsings() = usings |> Seq.sortBy (fun (t:UsingDirectiveSyntax) -> t.ToString()) |> Seq.toArray
+   member visitor.getCollectedUsings() = usings |> Seq.sortBy (fun (t:UsingDirectiveSyntax) -> t.ToString().TrimEnd(';')) |> Seq.toArray
 
    override visitor.VisitUsingDirective node =
       if node.UsingKeyword.HasLeadingTrivia then
-         let niceUsingToken = node.UsingKeyword.WithLeadingTrivia()
+         let rec cleanUpTrivia (l: SyntaxTrivia list) =
+            match l with
+            | hd :: nk :: tl when hd.Kind = SyntaxKind.SingleLineCommentTrivia && nk.Kind = SyntaxKind.EndOfLineTrivia -> hd :: nk :: cleanUpTrivia tl
+            | hd :: tl when hd.Kind = SyntaxKind.EndOfLineTrivia -> cleanUpTrivia tl
+            | hd :: tl -> hd :: cleanUpTrivia tl
+            | [] -> []
+         let niceUsingToken = node.UsingKeyword.WithLeadingTrivia(cleanUpTrivia (node.UsingKeyword.LeadingTrivia |> Seq.toList))
          let niceUsing = node.WithUsingKeyword niceUsingToken
          usings.Add niceUsing
       else
